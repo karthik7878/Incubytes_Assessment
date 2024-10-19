e2df7bfea428f8f4a2783043c6365a7f026f5389
karthik7878 <karthik.brichgunj@gmail.com>
2024-10-19 11:55:58 +0530

    Sum functionality which handles any amount of numbers in string

--- tejas_assignment/src/App.js
+++ tejas_assignment/src/App.js
@@ -1,8 +1,38 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputString, setInputString] = useState("");
+  const [sum, setSum] = useState(0);
+
+  const add = (numbers) => {
+    // Check if the input is not empty
+    if (numbers.trim() === "") {
+      setSum(0); // Set sum to 0 if input is empty
+      return;
+    }
+
+    const sumValue = numbers.split(",")
+      .reduce((acc, num) => {
+        const parsedNum = parseInt(num, 10);
+        return isNaN(parsedNum) ? acc : acc + parsedNum; // Ignore NaN values
+      }, 0);
+
+    setSum(sumValue);
+  }
+
+  const handleChange = (e) => {
+    setInputString(e.target.value);
+  }
+
   return (
-    <div className="App">s
+    <div className="App">
+      <div>
+        <label>Input String</label>
+        <input type="text" id="textbox" value={inputString} onChange={handleChange} />
+        <button onClick={() => add(inputString)}>Compute</button>
+        <span> Sum of the numbers is: {sum}</span>
+      </div>
     </div>
   );
 }
